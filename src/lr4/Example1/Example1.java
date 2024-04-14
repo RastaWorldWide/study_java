f3ac7e3d8004647c03c71e6211a500324f039c8a
Ilya Kulikov <boyindaspace@gmail.com>
2024-04-14 22:57:06 +0500

    lab4

---
+++ src/lr4/Example1/Example1.java
@@ -0,0 +1,12 @@
+package lr4.Example1;
+
+public class Example1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("Непроверяемая ошибка"); } catch (RuntimeException e) {
+            System.out.println("1 "+ e);
+        }
+        System.out.println("2");
+    }
+}
